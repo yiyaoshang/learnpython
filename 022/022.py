b3f8703db3c7478ab88ea87dc43ba3c8c6c299f4
heathu <876734392@qq.com>
2018-04-12 02:20:25 +0800

    第五部分

--- 022/022.py
+++ 022/022.py
@@ -3,17 +3,17 @@
 """
 author="heathu"
 date=20180410
-第 0005 题：你有一个目录，装了很多照片，把它们的尺寸变成都不大于 iPhone5 分辨率的大小。
+#iPhone 6、iPhone 6 Plus 早已上市开卖。请查看你写得 第 0005 题的代码是否可以复用。
 """
 import os
 from PIL import Image
 
-iPhone5_WIDTH = 1136
-iPhone5_HEIGHT = 640
+PHONE = {'iPhone5':(1136,640), 'iPhone6':(1134,750), 'iPhone6P':(2208,1242)}
 
-def resize_pic(path,new_path,width=iPhone5_WIDTH,height=iPhone5_HEIGHT):
+def resize_pic(path,new_path,phone_type):
     img = Image.open(path)
     w,h = img.size
+    width,height = PHONE[phone_type]
     if w > width:
         w = width
         h = w * height // h
@@ -23,13 +23,13 @@ def resize_pic(path,new_path,width=iPhone5_WIDTH,height=iPhone5_HEIGHT):
     new_img = img.resize((w,h),Image.ANTIALIAS)
     new_img.save(new_path)
 
-def walk_dir(path):
+def walk_dir(path,phonetype):
     for root,dirs,files in os.walk(path):
         for f_name in files:
             if f_name.lower().endswith('jpg'):
                 path_dst = os.path.join(root,f_name)
-                f_new_name = 'iPhone5_' + f_name
-                resize_pic(path=path_dst, new_path=f_new_name)
+                f_new_name = phonetype + f_name
+                resize_pic(path_dst, f_new_name,phonetype)
 
 if __name__=="__main__":
-    walk_dir('./')
+    walk_dir('./','iPhone6')
